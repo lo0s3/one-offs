41ffd3089c1193f3482c6c45febce9820965b222
matthewtrautman <33362536+matthewtrautman@users.noreply.github.com>
2017-11-03 17:41:17 -0700

    Update curl_me.sh

--- curl_me.sh
+++ curl_me.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while read ip ; do
-			echo $ip && curl -sS --connect-timeout .1 --max-time .3 -I http://$ip:8080/script
+	echo $ip && curl -sS --connect-timeout .1 --max-time .3 -I http://$ip:8080/script
 done < ip_list.txt
