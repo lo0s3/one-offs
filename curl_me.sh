9c0e3f040fd391a92e9af879d42b1bc22d4444b6
matthewtrautman <33362536+matthewtrautman@users.noreply.github.com>
2017-11-03 18:02:04 -0700

    Update curl_me.sh

--- curl_me.sh
+++ curl_me.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# This script will curl web servers through a proxy with a given input list
+# Ref: https://stackoverflow.com/questions/10929453/read-a-file-line-by-line-assigning-the-value-to-a-variable
+# Usage: ./curl_me.sh ip_list.txt
 
-while read ip ; do
-	echo $ip && curl -sS --connect-timeout .1 --max-time .3 -I http://$ip:8080/script
-done < ip_list.txt
+while IFS='' read -r ip || [[ -n "$ip" ]]; do
+	echo $ip && curl -I --proxy proxy_host:8080 -U user:yubikey -sS --connect-timeout 1 --max-time 1 http://$ip
+done < "$1"
